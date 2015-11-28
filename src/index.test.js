4278e1fe71b103051769102809d86a9edcb584e5
Jack Hinman <hinmanj@hinman.com>
2015-11-27 17:00:27 -0800

    begin adding tests

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var countryNames = require('./index.js');
+
+describe('country-names', function() {
+    it('should work!', function() {
+        expect(true).to.be.true;
+    });
+});
