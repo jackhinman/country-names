07f1a5646362747d8bbd807fe9006af69fa5d30f
Jack Hinman <hinmanj@hinman.com>
2015-11-27 20:58:52 -0800

    add additional tests

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,30 @@ var expect = require('chai').expect;
 var countryNames = require('./index.js');
 
 describe('country-names', function() {
-    it('should work!', function() {
-        expect(true).to.be.true;
+    describe('all', function() {
+        it('should be an array of strings', function() {
+            expect(countryNames.all).to.satisfy(isArrayOfStrings);
+
+            function isArrayOfStrings(array) {
+                return array.every(function(item) {
+                    return typeof item === 'string';
+                });
+            }
+        });
+
+        it('should contain `United States`', function() {
+            expect(countryNames.all).to.include('United States');
+        });
+    });
+
+    describe('random', function() {
+        it('should return a string', function() {
+            expect(countryNames.random()).to.satisfy(isMyString);
+
+            function isMyString(randomVal) {
+                var randomVal = randomVal;
+                return typeof randomVal === 'string';
+            }
+        });
     });
 });
